ef29f72f050dbfcac287f83c375248e48993de6d
caleb mwema <calebmwema65@gmail.com>
2024-01-06 18:24:12 +0300

    added listview and url config for the models

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, MembersView, IssuesView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("members/", MembersView.as_view(), name="members"),
+    path("issues/", IssuesView.as_view(), name="issues"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,20 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from books.models import Book
+from members.models import Member
+from issues.models import Issue
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class HomePageView(LoginRequiredMixin, TemplateView):
-    template_name = "home.html"
+class HomePageView(LoginRequiredMixin, ListView):
+    model = Book
+    template_name = "books.html"
+
+
+class MembersView(LoginRequiredMixin, ListView):
+    model = Member
+    template_name = "members.html"
+
+
+class IssuesView(LoginRequiredMixin, ListView):
+    model = Issue
+    template_name = "issues.html"
